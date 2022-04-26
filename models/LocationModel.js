66329026edb5702ee6d2100f2f752dc9b0f7deb8
David Feldman <djfeld01@gmail.com>
2022-04-26 10:41:21 -0400

    Updated Location With Current Occupancy

--- models/LocationModel.js
+++ models/LocationModel.js
@@ -1,39 +1,46 @@
 const mongoose = require('mongoose');
 
-const LocationSchema= new mongoose.Schema({
-    siteName:{
-        type: String,
-        required: [true, `Please Provide a siteName`]
-    },
-    streetAddres:{
-        type: String,
-    },
-    city: {
-        type: String
-    },
-    state: {
-        type: String
-    },
-    zipCode: {
-        type: String
-    },
-    phoneNumber: {
-        type: String
-    },
-    emailAddress: {
-        type: String
-    },
-    slLocationId: {
-        type: Number,
-        required: [true, `Please provide the Sitelink Location Id to slLocationId`]
-    },
-    slLocationLocal: {
-        type: String,
-    },
-    siteAbbreviation: {
-        type: String,
-        maxLength: 3
-    }
-})
+const LocationSchema = new mongoose.Schema({
+  siteName: {
+    type: String,
+    required: [true, `Please Provide a siteName`],
+  },
+  streetAddres: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  zipCode: {
+    type: String,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  emailAddress: {
+    type: String,
+  },
+  slLocationId: {
+    type: Number,
+    required: [true, `Please provide the Sitelink Location Id to slLocationId`],
+  },
+  slLocationLocal: {
+    type: String,
+  },
+  siteAbbreviation: {
+    type: String,
+    maxLength: 3,
+  },
+  paycorNumber: {
+    type: String,
+    maxLength: 4,
+  },
+  currentOccupancy: {
+    type: Number,
+  },
+});
 
 module.exports = mongoose.model('Location', LocationSchema);
--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const {
-    createLocation,
-    getLocation,
-    updateLocation,
-    getAllLocations,
-    deleteLocation
+  createLocation,
+  getLocation,
+  updateLocation,
+  updateLocationBySlLocationId,
+  getAllLocations,
+  deleteLocation,
 } = require('../controllers/locationController');
 
-router
-    .route('/')
-    .post(createLocation)
-    .get(getAllLocations)
+router.route('/').post(createLocation).get(getAllLocations);
 
 router
-    .route('/:id')
-    .get(getLocation)
-    .patch(updateLocation)
-    .delete(deleteLocation)
-    
+  .route('/:id')
+  .get(getLocation)
+  .patch(updateLocation)
+  .delete(deleteLocation);
+router.route('/sl/:id').patch(updateLocationBySlLocationId);
 module.exports = router;
